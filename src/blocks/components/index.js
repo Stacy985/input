463b06e7c49c90e5d2ea87d1c105d1eb4ab2c1c4
Stacy985 <anastasiya.disy@gmail.com>
2023-08-30 13:12:55 +0300

    исправление полузка

--- src/blocks/components/index.js
+++ src/blocks/components/index.js
@@ -17,45 +17,55 @@
 */
 
 
-
-
-
+// функция обнавления диапазона
 function updateRangeDisplay(sliderSection) {
+  // Поиск всех нужные элементы в разделе
   const rangeInputs = sliderSection.querySelectorAll("input._range-input[type='range']");
-  const lowerInput = sliderSection.querySelector("input._range-lower");
-  const upperInput = sliderSection.querySelector("input._range-upper");
-  const displayElement = sliderSection.querySelector("._rangeValues");
+  const lowerInput = sliderSection.querySelector("input._input-lower");
+  const upperInput = sliderSection.querySelector("input._input-upper");
+  const displayElement = sliderSection.querySelector("._range-values");
 
+  // Значение инпутоп === значения числам
   const lowerValue = parseFloat(lowerInput.value);
   const upperValue = parseFloat(upperInput.value);
 
+  // Input range соотвествует инпутам
   rangeInputs[0].value = lowerValue;
   rangeInputs[1].value = upperValue;
 
+  // Текст на странице === тестку текущими значениями
   displayElement.textContent = `${lowerValue} ₽ - ${upperValue} ₽`;
 }
 
+// Взаимодействие между ползунками и инпутами
 function setupRangeSlider(sliderSection) {
   const rangeInputs = sliderSection.querySelectorAll("input._range-input[type='range']");
-  const lowerInput = sliderSection.querySelector("input._range-lower");
-  const upperInput = sliderSection.querySelector("input._range-upper");
+  const lowerInput = sliderSection.querySelector("input._input-lower");
+  const upperInput = sliderSection.querySelector("input._input-upper");
 
+  //Обработчики событий для всех ползунков 
   rangeInputs.forEach(function (rangeInput) {
     rangeInput.addEventListener("input", function () {
+      //Значения инпутов ===  значению положения ползунков
+      lowerInput.value = rangeInputs[0].value;
+      upperInput.value = rangeInputs[1].value;
       updateRangeDisplay(sliderSection);
     });
   });
 
+
   lowerInput.addEventListener("input", function () {
+    rangeInputs[0].value = lowerInput.value;
     updateRangeDisplay(sliderSection);
   });
 
   upperInput.addEventListener("input", function () {
+    rangeInputs[1].value = upperInput.value;
     updateRangeDisplay(sliderSection);
   });
-
   updateRangeDisplay(sliderSection);
 }
+
 
 window.addEventListener("load", function () {
   const sliderSections = document.querySelectorAll("._range-slider");
